823d036a6300b164a0a2d4d3c2e2482cf41845c4
rayssaw28 <161396496+rayssaw28@users.noreply.github.com>
2025-05-06 20:08:09 -0300

    Add files via upload

---
+++ Atividade-N1/backend_n1/src/main/java/backend/PessoaDao.java
@@ -0,0 +1,10 @@
+package backend;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PessoaDao extends JpaRepository<Pessoa, Integer>{
+
+}
+    
